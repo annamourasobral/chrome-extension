12c58d769c350c4889af6a919a5ea029b016fa11
Anna Moura Sobral <annamourasobral@gmail.com>
2022-05-23 13:53:10 +0200

    input into link w/ template string + innerHTML

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
+    myLeads.push(inputEl.value);
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
 }
-
-
-
